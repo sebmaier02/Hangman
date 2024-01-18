ab27ce611422a3fcdb309b3da7821fcaf45d1d1e
Sebastian Maier <seb@MacBook-Pro-von-Sebastian.local>
2024-01-18 22:25:27 +0100

    iosApp

---
+++ GuessTheSong/GuessTheSong/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuessTheSong
+//
+//  Created by Sebastian Maier on 18.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheSong/GuessTheSong/GuessTheSongApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheSongApp.swift
+//  GuessTheSong
+//
+//  Created by Sebastian Maier on 18.01.24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheSongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
