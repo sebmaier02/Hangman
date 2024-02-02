0d7072a6c8feb991455afc105b3e648620ecf350
Sebastian Maier <seb@MacBook-Pro-von-Sebastian.local>
2024-02-02 10:11:04 +0100

    Design

--- GuessTheSong/GuessTheSong/ContentView.swift
+++ GuessTheSong/GuessTheSong/ContentView.swift
@@ -1,67 +1,30 @@
 import SwiftUI
 import WebKit
-import UIKit
 
 struct ContentView: View {
     var body: some View {
         VStack(spacing: 30) {
             Spacer()
+            
             Text("Welcome to Guess the Song!")
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundColor(.blue)
                 .multilineTextAlignment(.center)
                 .padding(.horizontal, 20)
+                .padding(.vertical, 40)
             
+            ButtonView(text: "Quick")
             
+            ButtonView(text: "Medium")
             
-            Button(action: {
-                // Action for Quick Game button
-            }) {
-                Text("Quick Game")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(20)
-                    .shadow(color: Color.blue.opacity(0.5), radius: 10, x: 0, y: 5)
-            }
-            
-            Button(action: {
-                // Action for Medium Game button
-            }) {
-                Text("Medium Game")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(20)
-                    .shadow(color: Color.blue.opacity(0.5), radius: 10, x: 0, y: 5)
-            }
-            
-            Button(action: {
-                // Action for Long Game button
-            }) {
-                Text("Long Game")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(20)
-                    .shadow(color: Color.blue.opacity(0.5), radius: 10, x: 0, y: 5)
-            }
+            ButtonView(text: "Infinity")
             
             Spacer()
         }
         .padding()
         .background(
-            LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.2), Color.white]), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.5), Color.white]), startPoint: .bottom, endPoint: .top)
                 .edgesIgnoringSafeArea(.all)
         )
     }
@@ -70,5 +33,24 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+private struct ButtonView: View {
+    var text: String
+    var body: some View {
+        Button(action: {
+            // Action for Medium Game button
+        }) {
+            Text(text)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .cornerRadius(20)
+                .shadow(color: Color.blue.opacity(0.5), radius: 10, x: 0, y: 5)
+        }
     }
 }
