e35eb16502866230fb14b3a20f00aa42f488cfa4
Sebastian Maier <seb@MacBook-Pro-von-Sebastian.local>
2024-02-02 19:34:21 +0100

    fortschrittOhneTobi

--- GuessTheSong/GuessTheSong/GameView.swift
+++ GuessTheSong/GuessTheSong/GameView.swift
@@ -10,12 +10,18 @@ import SwiftUI
 struct GameView: View {
     var rounds: Int
     @State var roundsPlayed: Int = 0
-    var score = 0
+    @State var score: Int = 0
+    @State var scoreThisRound: Int = 0
     @State var songName: String = ""
     @State var interpret: String = ""
     @State var isPlaying: Bool = true
     @State var submitted: Bool  = false
-    @State var firstRound: Bool  = true
+    @State var infinityRound: Bool = false
+    @State var end: Bool = false
+    @Environment(\.dismiss) var dismiss
+    @AppStorage("highscoreQuick") var highscoreQuick: Int = 0
+    @AppStorage("highscoreMedium") var highscoreMedium: Int = 0
+    @AppStorage("highscoreInfinity") var highscoreInfinity: Int = 0
     
     var body: some View {
         NavigationStack {
@@ -29,22 +35,18 @@ struct GameView: View {
                         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
                             isPlaying.toggle()
                         }
-                        if firstRound {
-                            firstRound.toggle()
-                        } else {
-                            roundsPlayed+=1
-                        }
+                        roundsPlayed+=1
                     }
                 } else {
                     if !submitted {
                         VStack {
                             Spacer()
                             
-                            Text("Please enter your awnsers")
+                            Text("Please enter your answer")
                                 .font(.title)
                                 .padding(.bottom, 50)
                             
-                            formTextField(titel: "Titel", placeholder: "titel", value: $songName)
+                            formTextField(titel: "Title", placeholder: "title", value: $songName)
                             
                             formTextField(titel: "Interpret", placeholder: "interpret", value: $interpret)
                             
@@ -73,40 +75,117 @@ struct GameView: View {
                     } else {
                         VStack (spacing: 30) {
                             Spacer()
-                            Text("Your Awnser").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                                .onAppear {
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                                        isPlaying.toggle()
-                                        submitted.toggle()
-                                    }
-                                }
+                            Text("Your Answer").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                            Text("Titel: " + songName).font(.title2)
+                            
+                            Text("Title: " + songName).font(.title2)
                             Text("Interpret: " + interpret).font(.title2)
                             
+                            Spacer()
+                            
                             Text("Correct Awnser").font(.title)
-                            Text("Titel: xxx" ).font(.title2)
+                            Text("Title: xxx" ).font(.title2)
                             Text("Interpret: xxx").font(.title2)
+                            
+                            Spacer()
+                            
+                            if scoreThisRound == 100 {
+                                Group {
+                                    Text("WOW you earned 100 points!!!")
+                                    Text("Your new score is \(score)")
+                                }
+                            } else if scoreThisRound == 50 {
+                                Text("You earned 50 points")
+                                Text("Your new score is \(score)")
+                            } else {
+                                Text("Maybe you should practice first.")
+                                Text("Score is still at \(score)")
+                            }
+                            
+                            if end {
+                                Spacer()
+                                Button(action: {
+                                    if rounds == 10 {
+                                        highscoreQuick = score
+                                    } else if rounds == 20 {
+                                        highscoreMedium = score
+                                    } else if rounds == -1 {
+                                        highscoreInfinity = score
+                                    }
+                                    dismiss()
+                                }) {
+                                    Text("End game")
+                                        .font(.title2)
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(.white)
+                                        .padding()
+                                        .frame(maxWidth: .infinity)
+                                        .background(Color.blue)
+                                        .cornerRadius(20)
+                                        .shadow(color: Color.blue.opacity(0.5), radius: 10, x: 0, y: 5)
+                                }
+                            }
                             
                             Spacer()
                         }
                         .padding()
+                        .onAppear{
+                            if songName == "xxx" && interpret == "xxx" {
+                                score += 100;
+                                scoreThisRound = 100;
+                            } else if songName == "xxx" || interpret == "xxx" {
+                                score += 50;
+                                scoreThisRound = 50;
+                            } else {
+                                scoreThisRound = 0;
+                                if infinityRound {
+                                    end = true
+                                }
+                            }
+                            
+                            if roundsPlayed == rounds && !infinityRound {
+                                end = true
+                            }
+                            
+                            if !end {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                    isPlaying.toggle()
+                                    submitted.toggle()
+                                }
+                            }
+                        }
                     }
                 }
             }
             .toolbar{
                 ToolbarItem(placement: .principal) {
-                    Text("Round: \(roundsPlayed) of \(rounds)")
-                        .padding()
-                        .foregroundStyle(Color.white)
-                        .font(.title3)
-                        .padding(.trailing, 5)
-                        .background(Color.blue)
-                        .cornerRadius(20)
-                        .shadow(color: Color.blue.opacity(0.5), radius: 10, x: 0, y: 5)
-                        .padding(.top, 80)
-                    
+                    if infinityRound {
+                        Text("Round: \(roundsPlayed)")
+                            .padding()
+                            .foregroundStyle(Color.white)
+                            .font(.title3)
+                            .padding(.trailing, 5)
+                            .background(Color.blue)
+                            .cornerRadius(20)
+                            .shadow(color: Color.blue.opacity(0.5), radius: 10, x: 0, y: 5)
+                            .padding(.top, 80)
+                    } else {
+                        Text("Round: \(roundsPlayed) of \(rounds)")
+                            .padding()
+                            .foregroundStyle(Color.white)
+                            .font(.title3)
+                            .padding(.trailing, 5)
+                            .background(Color.blue)
+                            .cornerRadius(20)
+                            .shadow(color: Color.blue.opacity(0.5), radius: 10, x: 0, y: 5)
+                            .padding(.top, 80)
+                    }
                 }
+            }
+        }
+        .onAppear{
+            if rounds < 0 {
+                infinityRound = true
             }
         }
     }
@@ -137,6 +216,5 @@ struct GameView: View {
 }
 
 #Preview {
-    GameView(rounds: 10)
+    GameView(rounds: -1)
 }
-
--- GuessTheSong/GuessTheSong/StartGameView.swift
+++ GuessTheSong/GuessTheSong/StartGameView.swift
@@ -11,12 +11,72 @@ struct StartGameView: View {
     var mode: String
     @State var rounds: Int = 0
     var description: String
+    @Environment(\.dismiss) var dismiss
+    @AppStorage("highscoreQuick") var highscoreQuick: Int = 0
+    @AppStorage("highscoreMedium") var highscoreMedium: Int = 0
+    @AppStorage("highscoreInfinity") var highscoreInfinity: Int = 0
     
     var body: some View {
-        NavigationStack {
-            Text(description)
+        VStack (alignment: .leading) {
+            Spacer()
             
-            NavigationLink(destination: GameView(rounds: rounds)) {
+            Text("Description")
+                .font(.largeTitle)
+                .padding(.bottom)
+            
+            Text(description)
+                .font(.title2)
+            
+            Spacer()
+            
+            Text("Rules")
+                .font(.largeTitle)
+                .padding(.bottom)
+            
+            if mode == "Quick" {
+                Group {
+                    Text("You play 10 rounds")
+                    Text("Title and interpret correct: 100 points")
+                    Text("Title ore interpret correct: 50 points")
+                    Text("Neither: 0 points")
+                }
+                .font(.title2)
+            } else if mode == "Medium" {
+                Group {
+                    Text("You play 20 rounds")
+                    Text("Title and interpret correct: 100 points")
+                    Text("Title ore interpret correct: 50 points")
+                    Text("Neither: 0 points")
+                }
+                .font(.title2)
+            } else if mode == "Infinity" {
+                Group {
+                    Text("There is no round limit!!!!")
+                    Text("Title and interpret correct: 100 points")
+                    Text("Title ore interpret correct: 50 points")
+                    Text("Neither: 0 points and Gameover!")
+                }
+                .font(.title2)
+            }
+            
+            Spacer()
+            
+            if mode == "Quick" {
+                Text("Highscore: \(highscoreQuick)")
+                    .font(.largeTitle)
+            } else if mode == "Medium" {
+                Text("Highscore: \(highscoreMedium)")
+                    .font(.largeTitle)
+            } else if mode == "Infinity" {
+                Text("Highscore: \(highscoreInfinity)")
+                    .font(.largeTitle)
+            }
+            
+            Spacer()
+            
+            NavigationLink{
+                GameView(rounds: rounds)
+            } label: {
                 Text("Start")
                     .font(.title2)
                     .fontWeight(.semibold)
@@ -27,6 +87,8 @@ struct StartGameView: View {
                     .cornerRadius(20)
                     .shadow(color: Color.blue.opacity(0.5), radius: 10, x: 0, y: 5)
             }
+            
+            Spacer()
         }
         .padding()
         .onAppear {
@@ -36,13 +98,10 @@ struct StartGameView: View {
                 rounds = 20
             } else if mode == "Infinity" {
                 rounds = -1
-            } else {
-                rounds = 10
             }
         }
     }
 }
-
 #Preview {
-    StartGameView(mode: "Quick", description: "lol")
+    StartGameView(mode: "Infinity", description: "lol")
 }
