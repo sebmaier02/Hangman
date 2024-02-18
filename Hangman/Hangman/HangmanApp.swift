f735563cc0baff430282324816594436412b033c
Sebastian Maier <seb@MBPvonSebastian.fritz.box>
2024-02-18 14:17:50 +0100

    gitignoreFix

---
+++ Hangman/Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by Sebastian Maier on 18.02.24.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
